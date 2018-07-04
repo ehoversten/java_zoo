ed94e931243bdfdc0249bfd16fcb3498a52fdfdf
ehoversten <ehoversten@gmail.com>
2018-07-04 10:20:11 -0400

    fixed BatTest class

--- BatTest.java
+++ BatTest.java
@@ -2,9 +2,13 @@ public class BatTest {
   public static void main(String[] args) {
     Bat vlad = new Bat();
     vlad.displayEnergy();
-    vlad.fly();
-    vlad.eatHumans();
     vlad.attackTown();
+    vlad.attackTown();
+    vlad.attackTown();
+    vlad.eatHumans();
+    vlad.eatHumans();
+    vlad.fly();
+    vlad.fly();
     vlad.displayEnergy();
   }
 }
